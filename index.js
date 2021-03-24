ffdd531e5ab1df9fc3cdf31206d2cdbd96f6399c
MysteriousK69 <daljeetmisidhu@gmail.com>
2021-03-25 02:27:27 +0530

    changed bg, added about us, contact us, features, reviews, privacy policy, terms.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./src/app')
 const PORT = 5000
-
-
+// ye xD
+// oof ig prettier broke some stuffs lol 
 app.listen(PORT,()=>{
     console.log(`[SERVER] Listening on port ${PORT}`)
 }) 
