4839f6b923836e5758a80ca4b8c3e085f28da5d4
Aypro <ayprogaming0@gmail.com>
2021-03-25 20:25:00 +0530

    Added \arc-sw.js to the route

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ppRouter = require('./routes/pp.js')
 var tosRouter = require('./routes/tos.js')
-
+var arcRouter = require('./routes/arc.js')
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +24,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', ppRouter);
 app.use('/', tosRouter);
+app.use('/', arcRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
